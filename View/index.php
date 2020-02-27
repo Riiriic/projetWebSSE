9783cbfda351ac1f5f8a426f0daaacde12531dd8
Khainy <lise.kabbache@gmail.com>
2020-02-28 00:09:42 +0100

    Tentative Page accueil

---
+++ View/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div id="test">
+      <h1>Bienvenue sur la simulation du plan ORSEC NOVI</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <button type="button" name="buttonPlay" id="btn-play">Jouer</button>
+    </div>
+  </body>
+</html>
