245e82921dd8235b8150cd75b9fbdc472ca06d6b
heluile <60426554+heluile@users.noreply.github.com>
2020-03-04 14:25:00 +0100

    Ajout de la classe Enum avec le code

---
+++ Model/Enum.php
@@ -0,0 +1,30 @@
+<?php
+namespace ProjetWeb;
+
+abstract class Enum
+{
+    protected $value;
+
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string)$this->value;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getConstList()
+    {
+        $rf = new \ReflectionClass(get_called_class());
+        return $rf->getConstants();
+    }
+
+?>
