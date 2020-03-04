1fa1854e124c3886d9e50b7db033e36c9831611c
heluile <60426554+heluile@users.noreply.github.com>
2020-03-04 17:20:07 +0100

    Ajout code pour la classe ChefPompier

---
+++ Model/ChefPompier.php
@@ -0,0 +1,41 @@
+<?php
+namespace projetWebSSE;
+
+class ChefPompier extends Utilisateur{
+  private PMA $pma;
+  private VehiculeUrgence $vu;
+  private Personnel $perso;
+
+  public function __construct(VehiculeUrgence $v, Personnel $p){
+    $this->pma=NULL;
+    $this->vu=$v;
+    $this->perso=$p;
+  }
+
+  public function getPma(){
+    return $this->pma;
+  }
+
+  public function getVu(){
+    return $this->vu;
+  }
+
+  public function getPerso(){
+    return $this->perso;
+  }
+
+  public function setPma(PMA $pma){
+    $this->pma=$pma;
+  }
+
+  public function setVu(VehiculeUrgence $v){
+    $this->vu=$v;
+  }
+
+  public function setPerso(Personnel $p){
+    $this->perso=$p;
+  }
+}
+
+
+?>
