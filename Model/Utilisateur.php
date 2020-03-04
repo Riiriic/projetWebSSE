8c5c6454be141cd70aad07bab7c766f1b908d897
heluile <60426554+heluile@users.noreply.github.com>
2020-03-04 15:05:21 +0100

    Ajout du code pour classe Utilisateur

---
+++ Model/Utilisateur.php
@@ -0,0 +1,45 @@
+<?php
+namespace ProjetWeb;
+
+abstract class Utilisateur{
+  private $_login;
+  private $_mdp;
+  private $_mail;
+
+  public function getLogin(){
+    return $this->_login;
+  }
+
+  public function getMdp(){
+    return $this->_mdp;
+  }
+
+  public function getMail(){
+    return $this->_mail;
+  }
+
+  public function setLogin($login){
+    if (!is_string($login)){
+      trigger_error('string requis', E_USER_WARNING);
+      return;
+    }
+    $this->_login=$login;
+  }
+
+  public function setMdp($mdp){
+    if (!is_string($mdp)){
+      trigger_error('string requis', E_USER_WARNING);
+      return;
+    }
+    $this->_mdp=$mdp;
+  }
+
+  public function setMail($mail){
+    if (!is_string($mail)){
+      trigger_error('string requis', E_USER_WARNING);
+      return;
+    }
+    $this->_mail=$mail;
+  }
+}
+?>
