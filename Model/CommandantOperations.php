649b81b61b7ad10757c817ecedd5e4b8c1f9284f
heluile <60426554+heluile@users.noreply.github.com>
2020-03-04 15:11:29 +0100

    Ajout code pour la classe CommandantOperations

--- Model/CommandantOperations.php
+++ Model/CommandantOperations.php
@@ -1,1 +1,34 @@
+<?php
+namespace ProjetWeb;
 
+class CommandantOperations extends Utilisateur{
+  private Personnel $_perso;
+  private Vehicule $_vehicule;
+
+  public function __construct($login, $mdp, $mail, Personnel $perso, Vehicule $vehicule){
+    $this->_login=$login;
+    $this->_mdp=$mdp;
+    $this->_mail=$mail;
+    $this->_perso=$perso;
+    $this->_vehicule=$vehicule;
+  }
+
+  public function getPerso(){
+    return $this->_perso;
+  }
+
+  public function getVehicule(){
+    return $this->_vehicule;
+  }
+
+  public function setPerso(Personnel $perso){
+    $this->_perso=$perso;
+  }
+
+  public function setVehicule(Vehicule $vehicule){
+    $this->_vehicule=$vehicule;
+  }
+}
+
+
+ ?>
