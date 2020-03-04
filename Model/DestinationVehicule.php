d86d7d57ebfbf59b99d7f47625cb282666bc159a
Riiriic <emeric.kilinc@gmail.com>
2020-03-04 16:41:26 +0100

    Ajout code pour la classe DestinationVehicule

---
+++ Model/DestinationVehicule.php
@@ -0,0 +1,20 @@
+<?php
+namespace projetWebSSE;
+
+class DestinationVehicule
+{
+    protected $_distance;
+
+    public function __construct(){
+        $this->_distance=NULL;
+    }
+
+    public function getDistance(){
+        return $this->_distance;
+    }
+
+    public function setDistance($dist){
+        $this->_distance=$dist;
+    }
+}
+?>
