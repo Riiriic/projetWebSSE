7efed69f0beb3e247d56686d88a6ca5826d2e228
heluile <60426554+heluile@users.noreply.github.com>
2020-03-04 15:26:09 +0100

    Ajout code pour la classe Unite

---
+++ Model/Unite.php
@@ -0,0 +1,68 @@
+<?php
+namespace ProjetWeb;
+
+abstract class Unite{
+  private $_numero;
+  private $_vitesseDeplacement;
+  private $_type;
+  private $_occupe;
+  private $_attribue;
+
+  public function getNumero(){
+    return $this->_numero;
+  }
+
+  public function setNumero($numero){
+    if (!is_int($numero)) {
+      trigger_error('Le numero doit être un nombre entier', E_USER_WARNING);
+    return;
+    }
+    $this->_numero = $numero;
+  }
+
+  public function getTempsDeploiement(){
+    return $this->$_vitesseDeplacement;
+  }
+
+  public function setTempsDeploiement($vitesse){
+    if (!is_int($vitesse)) {
+      trigger_error('La vitesse doit être un nombre entier', E_USER_WARNING);
+      return;
+    }
+    $this->$_vitesseDeplacement = $vitesse;
+  }
+
+  public function getType(){
+    return $this->$_type;
+  }
+
+  public function setType(TypesUnite $type){
+    $this->$_type = $type;
+  }
+
+  public function getOccupe(){
+    return $this->$_occupe;
+  }
+
+  public function setOccupe($bool){
+    if (!is_bool($bool)) {
+      trigger_error('boolean requis', E_USER_WARNING);
+      return;
+    }
+    $this->$_occupe = $bool;
+  }
+
+  public function getAttribue(){
+    return $this->$_attribue;
+  }
+
+  public function setOccupe($bool){
+    if (!is_bool($bool)) {
+      trigger_error('boolean requis', E_USER_WARNING);
+      return;
+    }
+    $this->$_attribue = $bool;
+  }
+}
+
+?>
