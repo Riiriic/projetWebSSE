fa4b10c49adaf6ea81b5d98c846b36a686bd401e
Kardya <44412682+Kardya@users.noreply.github.com>
2020-03-04 14:46:37 +0100

    création de la classe TypesEtat

---
+++ Model/TypesEtat.php
@@ -0,0 +1,16 @@
+<?php
+namespace ProjetWeb;
+
+use ProjetWeb\Enum;
+
+class TypesEtat extends Enum
+{
+    const UA = 'UA';
+
+    const UR = 'UR';
+
+    const DECEDE = 'Decede';
+
+    const TRAUMA = 'Trauma';
+}
+?>
