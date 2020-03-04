ee2c016abfa933b96ff6e9cfd0dfe0195235b157
Kardya <44412682+Kardya@users.noreply.github.com>
2020-03-04 15:58:14 +0100

    création de l'enumération des symptômes

---
+++ Model/TypeSymptome.php
@@ -0,0 +1,15 @@
+<?php
+namespace ProjetWeb;
+
+use ProjetWeb\Enum;
+
+class TypesSymptome extends Enum
+{
+    const MALTETE = 'mal de tête';
+
+    const VOMISSEMENT = 'vomissement';
+
+    const BRULURE = 'brûlure';
+}
+?>
+
